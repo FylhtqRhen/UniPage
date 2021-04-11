4049d56191a5a38c0463b5196b445e4ca2d5e2af
@rhenfylhtq <rhenfylhtq@gmail.com>
2021-04-11 14:35:15 +0300

    Commit to check local shit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -50,6 +50,24 @@ class ComposerAutoloaderInit9f7f75986abc9a26ee685934e3f5d722
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit9f7f75986abc9a26ee685934e3f5d722::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire9f7f75986abc9a26ee685934e3f5d722($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire9f7f75986abc9a26ee685934e3f5d722($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
